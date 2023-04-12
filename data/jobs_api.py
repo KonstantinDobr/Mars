98cdd8ffc59e357be1537d957a2739ebf8da830a
Konstantin_Dobr <snastink@yandex.ru>
2023-04-12 21:03:05 +0300

    Получение одной работы

--- data/jobs_api.py
+++ data/jobs_api.py
@@ -11,13 +11,26 @@ blueprint = flask.Blueprint(
 
 
 @blueprint.route('/api/jobs')
-def get_news():
+def get_jobs():
     db_sess = db_session.create_session()
-    news = db_sess.query(Job).all()
+    jobs = db_sess.query(Job).all()
     return flask.jsonify(
         {
             'jobs':
                 [item.to_dict(only=('id', 'job', 'work_size', 'collaborators', 'start_date', 'end_date', 'is_finished', 'team_leader'))
-                 for item in news]
+                 for item in jobs]
+        }
+    )
+
+
+@blueprint.route('/api/jobs/<int:jobs_id>', methods=['GET'])
+def get_one_news(jobs_id):
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Job).get(jobs_id)
+    if not jobs:
+        return flask.jsonify({'error': 'Not found'})
+    return flask.jsonify(
+        {
+            'jobs': jobs.to_dict(only=('id', 'job', 'work_size', 'collaborators', 'start_date', 'end_date', 'is_finished', 'team_leader'))
         }
     )
