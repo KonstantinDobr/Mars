2b951f4acc3de86aaf6795a5ed5a24242bdaed8e
Konstantin_Dobr <snastink@yandex.ru>
2023-04-12 21:25:54 +0300

    Добавление работы

--- data/jobs_api.py
+++ data/jobs_api.py
@@ -1,4 +1,5 @@
 import flask
+from flask import jsonify, request
 
 from . import db_session
 from .jobs import Job
@@ -14,7 +15,7 @@ blueprint = flask.Blueprint(
 def get_jobs():
     db_sess = db_session.create_session()
     jobs = db_sess.query(Job).all()
-    return flask.jsonify(
+    return jsonify(
         {
             'jobs':
                 [item.to_dict(only=('id', 'job', 'work_size', 'collaborators', 'start_date', 'end_date', 'is_finished', 'team_leader'))
@@ -28,9 +29,32 @@ def get_one_news(jobs_id):
     db_sess = db_session.create_session()
     jobs = db_sess.query(Job).get(jobs_id)
     if not jobs:
-        return flask.jsonify({'error': 'Not found'})
-    return flask.jsonify(
+        return jsonify({'error': 'Not found'})
+    return jsonify(
         {
             'jobs': jobs.to_dict(only=('id', 'job', 'work_size', 'collaborators', 'start_date', 'end_date', 'is_finished', 'team_leader'))
         }
     )
+
+
+@blueprint.route('/api/jobs', methods=['POST'])
+def create_jobs():
+    if not request.json:
+        return jsonify({'error': 'Empty request'})
+    elif not all(key in request.json for key in
+                 ['id', 'job', 'work_size', 'collaborators', 'start_date', 'end_date', 'is_finished', 'team_leader']):
+        return jsonify({'error': 'Bad request'})
+    db_sess = db_session.create_session()
+    job = Job(
+        id=request.json['id'],
+        job=request.json['job'],
+        work_size=request.json['work_size'],
+        collaborators=request.json['collaborators'],
+        start_date=request.json['start_date'],
+        end_date=request.json['end_date'],
+        is_finished=request.json['is_finished'],
+        team_leader=request.json['team_leader'],
+    )
+    db_sess.add(job)
+    db_sess.commit()
+    return jsonify({'success': 'OK'})
